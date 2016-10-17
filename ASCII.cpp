09c77110093c55f07ba98fa4acdb18d4bd789892
Steffen <steffen.moeller1993@gmx.net>
2016-10-17 11:44:50 +0200

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char anfangsbuchstabe = 'a';
+	cout << "Bitte geben sie einen Buchstaben ein:";
+	cin >> anfangsbuchstabe;
+	int x = anfangsbuchstabe;
+	cout << "Der Buchstabe " << anfangsbuchstabe << " hat die Position " << x << " im Alphabet.\n";
+		system("PAUSE");
+		return 0;
+}
