9bb2025c586ff08c0e2be1d27a455b1bff50bdfc
Steffen <steffen.moeller1993@gmx.net>
2016-10-19 20:45:59 +0200

    Update ASCII.cpp

--- ASCII.cpp
+++ ASCII.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 	char anfangsbuchstabe = 'a';
-	cout << "Bitte geben sie einen Buchstaben ein:";
+	cout << "Bitte geben sie einen Buchstaben ein: ";
 	cin >> anfangsbuchstabe;
 	int x = anfangsbuchstabe;
+	x = int(anfangsbuchstabe) - '`';
 	cout << "Der Buchstabe " << anfangsbuchstabe << " hat die Position " << x << " im Alphabet.\n";
-		system("PAUSE");
-		return 0;
+	system("PAUSE");
+	return 0;
 }
