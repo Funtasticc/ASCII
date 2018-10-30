bb6b61f5365b3a8328ad4786cdfa86113df47fa8
Steffen <steffen.moeller1993@gmx.net>
2018-10-30 09:19:09 +0100

    Update ASCII Alphabet_02_01.cpp

--- ASCII Alphabet_02_01.cpp
+++ ASCII Alphabet_02_01.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main()
 {
-	char anfangsbuchstabe = 'a';
-	cout << "Bitte geben sie einen Buchstaben ein: ";
-	cin >> anfangsbuchstabe;
-	int x = anfangsbuchstabe;
-	x = int(anfangsbuchstabe) - '`';
-	cout << "Der Buchstabe " << anfangsbuchstabe << " hat die Position " << x << " im Alphabet.\n";
+	char buchstabe;
+
+	int posi;
+
+	cout << "Bitte geben sie einen Buchstaben ein: ? ";
+	cin >> buchstabe;
+
+	posi = buchstabe - ('a' - 1);
+
+	cout << "Der Buchstabe hat die Position " << posi << " im Alphabet" << endl;
+
 	system("PAUSE");
+
 	return 0;
+
 }
